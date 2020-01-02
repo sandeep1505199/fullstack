ef0e9523a654dc8b9b020a3905f1f0f8b62f7dea
sandeep divakaruni <sandeep1505199@gmail.com>
2020-01-02 11:59:02 +0530

    fixing server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const port = process.env.PORT||3000
 const path = require('path')
 
 app.use(express.static(__dirname + '/dist/ngproject'))
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/dist/ngproject/index.html'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/dist/ngproject/index.html')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
